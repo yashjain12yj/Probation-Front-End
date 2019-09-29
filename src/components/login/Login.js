54b96085a3bcb82310eae65013ccd045547c6629
Yash Jain <yashjain12yj@gmail.com>
2019-09-30 00:43:43 +0530

    Auth in progress

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,30 +1,76 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import axios from 'axios';
 
 import './Login.css';
 
-function Login() {
+
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      usernameOrEmail:'',
+      password:''
+    }
+   }
+
+   handleClick(event){
+     var apiBaseUrl = "http://localhost:5000/api/auth/";
+     var payload={
+     "usernameOrEmail":this.state.usernameOrEmail,
+     "password":this.state.password
+     }
+     console.info(payload);
+     axios.post(apiBaseUrl+'signin', payload)
+        .then(function (response) {
+          console.log(response);
+          if(response.data.code == 200){
+            console.log("Login successfull");
+          }
+          else if(response.data.code == 204){
+              console.log(response.data.message)
+              alert("username password do not match")
+          }
+          else{
+              console.log(response.data.message)
+              alert("Username does not exist");
+          }
+         })
+        .catch(function (error) {
+            console.log(error);
+         });
+   }
+
+render(){
   return (
     <div className="container login-form">
     	<h2 className="login-heading">Login</h2>
       <form className="">
         <div className="form-group row">
-          <label for="staticEmail" className="offset-md-3 col-sm-2 col-form-label">Email</label>
+          <label htmlFor="staticUsernameOrEmail" className="offset-md-3 col-sm-2 col-form-label">Username/Email</label>
           <div className="col-sm-4">
-            <input type="text" className="form-control" id="staticEmail" placeholder="email@example.com" />
+            <input
+              type="text"
+              className="form-control"
+              onChange = {(event) => this.setState({usernameOrEmail:event.target.value})}
+              />
           </div>
         </div>
         <div className="form-group row">
-          <label for="inputPassword" className="offset-md-3 col-sm-2 col-form-label">Password</label>
+          <label htmlFor="inputPassword" className="offset-md-3 col-sm-2 col-form-label">Password</label>
           <div className="col-sm-4">
-            <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+            <input
+              type="password"
+              className="form-control"
+              onChange = {(event) => this.setState({password:event.target.value})}
+            />
           </div>
         </div>
       </form>
-    	<button className="btn btn-primary">Login</button>
-      <span class="donotHaveAccountLogin">If you do not have a account <Link to="/signup">Register</Link></span>
+    	<button className="btn btn-primary" onClick={(event) => this.handleClick(event)}>Login</button>
+      <span className="donotHaveAccountLogin">If you do not have a account <Link to="/signup">Register</Link></span>
     </div>
-  );
+  )};
 }
 
 export default Login;
--- src/components/login/Signup.js
+++ src/components/login/Signup.js
@@ -2,48 +2,131 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 import './Signup.css';
+import axios from 'axios';
 
+class Signup extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      name:'',
+      username:'',
+      email:'',
+      password:'',
+      confirmPassword:'',
+      mobile:''
+    }
+   }
 
-function Signup() {
-  return (
-    <div className="container signup-form">
-    	<h2 className="signup-heading">Register</h2>
-      <form className="">
-      <div className="form-group row">
-        <label for="staticEmail" className="offset-md-3 col-sm-2 col-form-label">Name</label>
-        <div className="col-sm-4">
-          <input type="text" className="form-control" id="name" placeholder="xyz" />
-        </div>
+  handleClick(event){
+    console.log("Clicked")
+    var payload = {
+      "name":this.state.name,
+      "username":this.state.username,
+      "email":this.state.email,
+      "password":this.state.password,
+    }
+
+    payload = {
+      "name": "Rashi Neema",
+    	"username": "rashineema",
+    	"email": "rashineema@gmail.com",
+    	"password": "neemarashi"
+    }
+
+    console.log(payload);
+    var apiBaseUrl = "http://localhost:5000/api/auth/";
+
+    axios.post(apiBaseUrl+'signup', payload)
+       .then(function (response) {
+         console.log(response);
+         if(response.status == 200){
+           console.log("Signup successfull");
+         }
+         else if(response.status == 204){
+             console.log(response.data.message)
+             alert("Kuch to hua")
+         }
+         else{
+             console.log(response.data.message)
+             alert("Kuch bhi ho gya");
+         }
+        })
+       .catch(function (error) {
+           console.log(error);
+        });
+
+  }
+  render(){
+    return (
+      <div className="container signup-form">
+      	<h2 className="signup-heading">Register</h2>
+        <form className="">
+          <div className="form-group row">
+            <label htmlFor="staticName" className="offset-md-3 col-sm-2 col-form-label">Name</label>
+            <div className="col-sm-4">
+              <input
+                type="text"
+                className="form-control"
+                onChange = {(event) => this.setState({name:event.target.value})}
+              />
+            </div>
+          </div>
+          <div className="form-group row">
+            <label htmlFor="staticUsername" className="offset-md-3 col-sm-2 col-form-label">Username</label>
+            <div className="col-sm-4">
+              <input
+                type="text"
+                className="form-control"
+                onChange = {(event,newValue) => this.setState({username:event.target.value})}
+              />
+            </div>
+          </div>
+          <div className="form-group row">
+            <label htmlFor="staticEmail" className="offset-md-3 col-sm-2 col-form-label">Email</label>
+            <div className="col-sm-4">
+              <input
+                type="text"
+                className="form-control"
+                onChange = {(event,newValue) => this.setState({email:event.target.value})}
+              />
+            </div>
+          </div>
+          <div className="form-group row">
+            <label htmlFor="inputPassword" className="offset-md-3 col-sm-2 col-form-label">Password</label>
+            <div className="col-sm-4">
+              <input
+                type="password"
+                className="form-control"
+                onChange = {(event,newValue) => this.setState({password:event.target.value})}
+              />
+            </div>
+          </div>
+          <div className="form-group row">
+            <label htmlFor="inputConfirmPassword" className="offset-md-3 col-sm-2 col-form-label">Confirm Password</label>
+            <div className="col-sm-4">
+              <input
+                type="password"
+                className="form-control"
+                onChange = {(event,newValue) => this.setState({confirmPassword:event.target.value})}
+              />
+            </div>
+          </div>
+          <div className="form-group row">
+            <label htmlFor="mobile" className="offset-md-3 col-sm-2 col-form-label">Mobile</label>
+            <div className="col-sm-4">
+              <input
+                type="text"
+                className="form-control"
+                onChange = {(event,newValue) => this.setState({mobile:event.target.value})}
+              />
+            </div>
+          </div>
+        </form>
+      	<button className="btn btn-primary" onClick={(event) => this.handleClick(event)}>Register</button>
+        <span className="alreadyHaveAccountSignup">If you already have a account <Link to="/login">Login</Link></span>
       </div>
-        <div className="form-group row">
-          <label for="staticEmail" className="offset-md-3 col-sm-2 col-form-label">Email</label>
-          <div className="col-sm-4">
-            <input type="text" className="form-control" id="staticEmail" placeholder="email@example.com" />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label for="inputPassword" className="offset-md-3 col-sm-2 col-form-label">Password</label>
-          <div className="col-sm-4">
-            <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label for="inputConfirmPassword" className="offset-md-3 col-sm-2 col-form-label">Confirm Password</label>
-          <div className="col-sm-4">
-            <input type="password" className="form-control" id="inputConfirmPassword" placeholder="Confirm Password" />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label for="mobile" className="offset-md-3 col-sm-2 col-form-label">Mobile</label>
-          <div className="col-sm-4">
-            <input type="text" className="form-control" id="mobile" />
-          </div>
-        </div>
-      </form>
-    	<button className="btn btn-primary">Register</button>
-      <span class="alreadyHaveAccountSignup">If you already have a account <Link to="/login">Login</Link></span>
-    </div>
-  );
+    )
+  };
 }
 
 export default Signup;
